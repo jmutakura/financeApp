0ddadcfa0d95b67182206170228bff5ed3452bd9
jmutakura <jmutakura@gmail.com>
2022-01-13 22:35:22 -0600

    Managed to get listview working

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,41 @@
 import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: ListViewBUilder(),
+    );
+  }
+}
+
+// ignore: use_key_in_widget_constructors
+class ListViewBUilder extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("List"),
+      ),
+      body: ListView.builder(
+        itemCount: 20,
+        itemBuilder: (BuildContext context, int index) {
+          return ListTile(
+            leading: Icon(Icons.list),
+            trailing: Text(
+              "GFG",
+            ),
+            title: Text("List Item $index"),
+          );
+        },
+      ),
+    );
+  }
+}

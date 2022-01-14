0bea4c3ffc16867cd34bbe8673211ee2d5064f0d
jmutakura <jmutakura@gmail.com>
2022-01-13 23:39:52 -0600

    Home screen implementation

--- lib/ListViewBuilder.dart
+++ lib/ListViewBuilder.dart
@@ -1,28 +1,24 @@
 import 'package:flutter/material.dart';
-import 'ListViewBuilder.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Home(),
-    );
-  }
-}
-
-class Home extends StatelessWidget {
+// ignore: use_key_in_widget_constructors
+class ListViewBUilder extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Home"),
+        title: Text("List View"),
+      ),
+      body: ListView.builder(
+        itemCount: 20,
+        itemBuilder: (BuildContext context, int index) {
+          return ListTile(
+            leading: Icon(Icons.list),
+            trailing: Text(
+              "GFG",
+            ),
+            title: Text("List Item $index"),
+          );
+        },
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
